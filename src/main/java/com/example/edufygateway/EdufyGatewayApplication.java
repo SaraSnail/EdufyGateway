4de621e02004ed114457ce97339b562daa8cf82a
Sara <sara.andersson185@gmail.com>
2025-10-28 15:02:26 +0100

    2025.10.28 First commit

---
+++ src/main/java/com/example/edufygateway/EdufyGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.edufygateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdufyGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdufyGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/edufygateway/EdufyGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.edufygateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdufyGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
